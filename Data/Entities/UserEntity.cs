e6a006f2f744543356310803cc69937a1cebfc0c
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-20 15:07:14 +0200

    User entity created

---
+++ Data/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Data.Entities;
+
+public class UserEntity
+{
+    public IFormFile? ProfileImage { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
