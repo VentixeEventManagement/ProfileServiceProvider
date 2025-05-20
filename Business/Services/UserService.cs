de913ce7c606c4dc7c40e13f8f8b0ac6c2066e0d
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-20 16:34:19 +0200

    UserService created

---
+++ Business/Services/UserService.cs
@@ -0,0 +1,10 @@
+﻿using Data.Interfaces;
+
+namespace Business.Services;
+
+public class UserService(IUserRepository userRepository)
+{
+    private readonly IUserRepository _userRepository = userRepository;
+
+    
+}
