0083d26b8271859c942375059bbe2853fde6d25a
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-20 16:41:59 +0200

    ResponseResult created

---
+++ Business/Models/ResponseResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Business.Models;
+
+public class ResponseResult
+{
+    public bool Succeeded { get; set; }
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+}
+
+public class ResponseResult<T> : ResponseResult 
+{
+    public T? Result { get; set; }
+}
