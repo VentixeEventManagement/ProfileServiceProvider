99593cbe6f89d05d67567b31a013eb001c909a88
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-20 14:56:15 +0200

    User model created

---
+++ Business/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Business.Models;
+
+public class User
+{
+    public string UserId { get; set; } = null!;
+    public IFormFile? ProfileImage { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set;}
+
+}
