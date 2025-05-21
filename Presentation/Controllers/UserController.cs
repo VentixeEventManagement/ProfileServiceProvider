36ad77fdec833207b6fcee48903b49f797020cee
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-21 10:42:01 +0200

    Add Swagger support and user registration endpoint
    - Updated `Presentation.csproj` to include `SwashBuckle.AspNetCore` for API documentation and removed the `Controllers` folder reference.
    - Enhanced `Program.cs` with database context configuration for SQL Server and added necessary namespaces for data access.
    - Introduced `UserController` to handle user registration with a `Create` method that validates input and interacts with the `IUserService`.

---
+++ Presentation/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Interfaces;
+using Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IUserService userService) : ControllerBase
+{
+    private readonly IUserService _userService = userService;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(UserRegistrationForm form)
+    {
+        if (!ModelState.IsValid) 
+            return BadRequest(ModelState);
+
+        var result = await _userService.AddUserInfoasync(form);
+        if (!result.Succeeded)
+            return BadRequest(result.Message);
+
+        return Ok(result);
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,13 +1,17 @@
 using Business.Interfaces;
 using Business.Services;
+using Data.Contexts;
 using Data.Interfaces;
 using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Presentation.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors(option => { option.AddPolicy("AllowAll", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());  });
+builder.Services.AddDbContext<DataContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("ProfileDatabase")));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
