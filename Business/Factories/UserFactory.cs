cdc5f003917a8be746eb0d2656551cd943ae0588
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-21 09:34:33 +0200

    Add user registration functionality and models
    - Updated UserService to include AddUserInfoasync method for handling user registration with error handling.
    - Modified UserEntity to add Id property as the primary key.
    - Introduced UserFactory for creating UserEntity from UserRegistrationForm.
    - Added UserRegistrationForm model for user registration data transfer.

---
+++ Business/Factories/UserFactory.cs
@@ -0,0 +1,20 @@
+﻿using Business.Models;
+using Data.Entities;
+
+namespace Business.Factories;
+
+public static class UserFactory
+{
+    public static UserEntity Create(UserRegistrationForm formData)
+    {
+        var entity = new UserEntity
+        {
+            UserId = formData.UserId,
+            ProfileImage = formData.ProfileImage,
+            FirstName = formData.FirstName,
+            LastName = formData.LastName,
+        };
+
+        return entity;
+    }
+}
--- Business/Services/UserService.cs
+++ Business/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using Data.Interfaces;
+﻿using Business.Factories;
+using Business.Models;
+using Data.Interfaces;
 
 namespace Business.Services;
 
@@ -6,5 +8,27 @@ public class UserService(IUserRepository userRepository)
 {
     private readonly IUserRepository _userRepository = userRepository;
 
-    
+    public async Task<ResponseResult> AddUserInfoasync(UserRegistrationForm form)
+    {
+        try
+        {
+            var entity = UserFactory.Create(form);
+            if (entity == null)
+            {
+                return new ResponseResult { Succeeded = false, Message = "Invalid registration form." };
+            }
+
+            var result = await _userRepository.AddAsync(entity);
+            if (!result)
+            {
+                return new ResponseResult { Succeeded = false, Message = "Something went wrong with creation.", StatusCode = 400 };
+            }
+
+            return new ResponseResult { Succeeded = true, StatusCode = 200, Message = "Profile information was created successfully." };
+
+        } catch (Exception ex)
+        {
+            return new ResponseResult { Succeeded = false, Message = ex.Message };
+        }
+    }
 }
